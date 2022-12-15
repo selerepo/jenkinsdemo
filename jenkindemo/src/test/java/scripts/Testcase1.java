23dbe0799fbe8a596d0349ae7987fd6ed0660eda
hamsa <hamsa@DESKTOP-CSDR9DJ.home>
2022-12-15 12:27:26 -0500

    push project jenkindemo to repository

---
+++ jenkindemo/src/test/java/scripts/Testcase1.java
@@ -0,0 +1,12 @@
+package scripts;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase1 {
+	@Test
+		public void tc1() {
+		Reporter.log("hellojava",true);
+	}
+
+}
+
---
+++ jenkindemo/src/test/java/scripts/Testcase2.java
@@ -0,0 +1,13 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase2 {
+@Test
+
+public void tc2() {
+	Reporter.log("hello selenium",true);
+}
+
+}
